168c1fcac6f02ae9846467b6ac4b80f85e1fcefe
HemanthThummepalli-Git <hemanththummepalli@gmail.com>
2025-04-01 11:46:46 +0530

    21stcommit

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -60,7 +60,7 @@ const NavBar = () => {
 
   {/* Login / Logout Buttons */}
   <Nav className="text-center ">
-    {currentUser ? (
+    {currentUser.token==localStorage.getItem("token") ? (
       <div className="d-flex gap-3 align-items-center width-100">
         <Nav.Link as={NavLink} to="/dashboard" className="nav-link-glow d-flex align-items-center justify-content-between">
        <FaTachometerAlt className="me-1" /><div> Dashboard </div>
